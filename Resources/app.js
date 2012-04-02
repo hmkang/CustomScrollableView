2c95ab92f6e4f8db3d346e2519a808102e0499b9
hmkang <hmkang@inervit.com>
2012-04-02 17:54:03 +0900

    initial

---
+++ Resources/app.js
@@ -0,0 +1,33 @@
+var Custom = require('./ui/CustomScrollableView');
+
+var win = Ti.UI.createWindow({
+	title: 'Custom Scrollable View',
+	backgroundColor: '#000'
+});
+
+var view1 = Ti.UI.createView({
+	backgroundColor: 'blue'
+});
+
+var view2 = Ti.UI.createView({
+	backgroundColor: 'red'
+});
+
+var view3 = Ti.UI.createView({
+	backgroundColor: 'yellow'
+});
+
+var view = Custom.createScrollableView({
+	views: [view1, view2, view3],
+	top: 10,
+	left: 10,
+	bottom: 10,
+	right: 10,
+	borderWidth: 5,
+	borderRadius: 10,
+	borderColor: '#aaa',
+	pagingControlPosition: 'bottom'
+});
+
+win.add(view);
+win.open();
---
+++ Resources/ui/CustomScrollableView.js
@@ -0,0 +1,78 @@
+function merge(a, b) {
+	if (a && b) {
+		for (var key in b) {
+			a[key] = b[key];
+		}
+	}
+	return a;
+}
+
+var dotColor = {
+	active: '#999',
+	inactive: '#555'
+};
+
+var activeViewIndex = 0;
+function changeActive(view, length, newIndex){
+	if(view.children){
+		view.children[activeViewIndex].borderColor = dotColor.inactive;
+		view.children[newIndex].borderColor = dotColor.active;
+		activeViewIndex = newIndex;
+	}else{
+		for(var i=0; i<length; i++){
+			var borderColor = i==0 ? dotColor.active : dotColor.inactive;
+			var dot = Ti.UI.createView({
+				left: 4,
+				right: 4,
+				width: 8,
+				height: 8,
+				borderWidth: 5,
+				borderRadius: 5,
+				borderColor: borderColor	
+			});
+			view.add(dot);
+		}
+	}
+}
+
+function createScrollableView(o) {
+	o = o || {};
+	o.showPagingControl = false; // hide default paging control 
+	o.views = o.views || [];
+	o.top = o.top || 0;
+	o.bottom = o.bottom || 0;
+
+	// Create paging control
+	var pagingControlView = Ti.UI.createView({
+		layout: "horizontal",
+		height: 20,
+		width: "auto"
+	});
+	
+	// Set default paging control position	
+	var pos = o.pagingControlPosition;
+	pos = pos=='top' || pos=='bottom' ? pos : 'bottom';  
+	o.pagingControlPosition = pos;
+	
+	// Determin position for paging control
+	pagingControlView[pos] = o[pos];
+	o[pos] += pagingControlView.height + (pos=='bottom' ? 10 : 0);
+	
+	// Set initial paging control
+	changeActive(pagingControlView, o.views.length, 0);
+	
+	// Create scrollable view
+	var view = Ti.UI.createScrollableView(o);
+	view.addEventListener('scroll', function(e){
+		changeActive(pagingControlView, o.views.length, e.currentPage);
+	});
+	
+	// Create container including scrollable view and page control
+	var container = Ti.UI.createView();
+	container.add(view);
+	container.add(pagingControlView);
+	
+	return container;
+}
+
+exports.createScrollableView = createScrollableView;
